789496eefcbd9574a8f0963b6a8fc138d71402bf
salommenbulbosit <abdulbositmirgani@gmail.com>
2025-06-26 20:45:48 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let name = prompt('ismingizni kiriting')
+let harf = prompt('biron bir harf kiriting')
+
+if(name.includes(harf)){
+    alert('sizning ismingiz ichida' +harf +' harifi bor')
+}else{
+        alert('sizning ismingiz ichida' +harf +' harifi yoq')
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
